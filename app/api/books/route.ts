dc97a1c116538cb5801ef78d9c4e8e8e1d487c39
JJleem <leemjaejun@gmail.com>
2024-12-16 09:37:08 +0900

    api Url edit +++

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY}`;
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`;
   try {
     const response = await fetch(apiUrl);
 
